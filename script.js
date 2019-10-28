67cefbd2f39e0ef02d08e80be8cd2031a5b3ad4b
edwinvb <evb1915@gmail.com>
2019-10-27 22:44:39 -0500

    try

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function() {
 		let copilotName = document.querySelector('input[name=copilotName]');
 		let fuelLevel = document.querySelector('input[number=fuelLevel]');
 		let cargoMass = document.querySelector('input[number=cargoMass]');
-		if (pilotName.value === 'Edwin') {
+		if (pilotName.value == Number) {
 			alert('WHHHHHAAAAAAAAATTTTTT');
 		}
 		if (pilotName.value === '' || copilotName.value === '' || fuelLevel.value === '' || cargoMass.value === '') {
