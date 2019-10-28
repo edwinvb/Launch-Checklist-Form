1207b77cd7d8f65d2e2e5590441e91bcabf53b43
edwinvb <evb1915@gmail.com>
2019-10-27 22:57:32 -0500

    try

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ window.addEventListener('load', function() {
 		if (pilotName.value === '' || copilotName.value === '' || fuelLevel.value === '' || cargoMass.value === '') {
 			alert('All fields are required!');
 		}
-		if (isNaN(pilotName.value)) {
+		if (isNaN(pilotName)) {
 			alert('OMG PUT IN A NAME');
 		}
 	});
