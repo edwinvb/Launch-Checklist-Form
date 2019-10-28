13a86a3ab9d23949fcc699d80cf13bd5caf0c329
edwinvb <evb1915@gmail.com>
2019-10-27 23:11:30 -0500

    try

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ window.addEventListener('load', function() {
 	form.addEventListener('submit', function(event) {
 		let pilotName = document.querySelector('input[name=pilotName]');
 		let copilotName = document.querySelector('input[name=copilotName]');
-		let fuelLevel = document.querySelector('input[number=fuelLevel]');
-		let cargoMass = document.querySelector('input[number=cargoMass]');
+		let fuelLevel = document.querySelector('input[name=fuelLevel]');
+		let cargoMass = document.querySelector('input[name=cargoMass]');
 
 		if (pilotName.value === '' || copilotName.value === '' || fuelLevel.value === '' || cargoMass.value === '') {
 			alert('All fields are required!');
