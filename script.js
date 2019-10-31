b03b821609871e66422b0216ff8e1c754df3673e
edwinvb <evb1915@gmail.com>
2019-10-31 17:58:06 -0500

    hopefully final commit

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ window.addEventListener('load', function() {
 			event.preventDefault();
 		}
 		if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
-			alert('Fuel Level and Cargo Mass must be a number');
+			alert('Fuel and Cargo must be numerals');
 			event.preventDefault();
 		}
 		document.getElementById('pilotStatus').innerHTML = `Pilot ${pilotName.value} is ready for launch`;
@@ -20,12 +20,19 @@ window.addEventListener('load', function() {
 		let launchStatus = document.getElementById('launchStatus');
 		let cargoStatus = document.getElementById('cargoStatus');
 		console.log(pilotName.value, copilotName.value, fuelLevel.value, cargoMass.value);
-		if (fuelLevel.value <= 9999 || cargoMass > 10000) {
+		if (fuelLevel.value < 10000 || cargoMass > 10000) {
 			missionUpdate.style = 'visibility: visible';
 			launchStatus.style = 'color: red';
 			launchStatus.innerHTML = 'SHUTTLE NOT READY FOR LAUNCH';
 			fuelLevel.value <= 10000 ? (fuelStatus.innerHTML = 'Fuel level not high enough for launch') : null;
 			cargoMass.value >= 10000 ? (cargoStatus.innerHTML = 'Cargo Mass too High for Liftoff') : null;
+		}
+		if (fuelLevel.value > 9999 || cargoMass < 9999) {
+			missionUpdate.style = 'visibility: visible';
+			launchStatus.style = 'color: green';
+			launchStatus.innerHTML = 'SHUTTLE IS READY FOR LAUNCH';
+			fuelLevel.value >= 10000 ? (fuelStatus.innerHTML = 'Fuel level is high enough for launch') : null;
+			cargoMass.value <= 10000 ? (cargoStatus.innerHTML = 'Cargo Mass adequate for Liftoff') : null;
 		}
 		fetch('https://handlers.education.launchcode.org/static/planets.json').then(function(response) {
 			//  const jsonPlanets = response.json.then(function(json);
@@ -50,19 +57,3 @@ window.addEventListener('load', function() {
 		event.preventDefault();
 	});
 });
-{
-	/* </html> Write your JavaScript code here! */
-}
-
-{
-	/* /* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">  */
-}
